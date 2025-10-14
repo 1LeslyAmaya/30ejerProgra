176d012ee2f478ab4e6d3335a8d9263807dcd86b
lESLY AMAYA <lamayal@miumg.edu.gt>
2025-10-14 10:43:12 -0600

    3

---
+++ 3.py
@@ -0,0 +1,8 @@
+num1 = int(input ("Ingrese un numero: "))
+num2 = int(input("Ingrese un numero: "))
+num3 = int(input("Ingrese un numero: "))
+if num1 > num2 and num1 > num3: 
+    print("El numero mayor es :", num1)
+elif num2 > num1 and num2 > num3:
+    print("El numero mayor es:", num2)
+else: print("El numero mayor es:", num3)
