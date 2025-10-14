d880b9aa1de3c04a98cd222e51faf0503c2edd66
lESLY AMAYA <lamayal@miumg.edu.gt>
2025-10-14 12:12:23 -0600

    5

---
+++ 5.py
@@ -0,0 +1,4 @@
+letra = input("Ingrese una letra: " ).lower()
+if letra =="a" or letra =="e" or  letra =="i" or letra =="o" or letra =="u":
+   print ("La letra es una vocal")
+else: print ("La letra es una consonate")
