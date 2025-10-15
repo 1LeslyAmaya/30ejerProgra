c0f3b3de4593943ad433fda88402226f372576ce
lESLY AMAYA <lamayal@miumg.edu.gt>
2025-10-15 14:45:46 -0600

    8

---
+++ 8.py
@@ -0,0 +1,7 @@
+numero = int(input("Ingrese un número: "))
+if (numero % 5 == 0):
+    print ("Es multiplo de 5")
+elif (numero % 7 == 0):
+    print ("Es multiplo de 7")
+else: 
+    print ("El numero no es multiplo de  5 ni de 7")
