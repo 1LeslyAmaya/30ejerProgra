f05e4015ee539ef69640e8d77f50eeb63f2a5eba
lESLY AMAYA <lamayal@miumg.edu.gt>
2025-10-17 15:52:31 -0600

    15

---
+++ 15.py
@@ -0,0 +1,10 @@
+suma = 0
+
+while suma <= 100:
+    numero = int(input("Ingrese un número: "))
+    suma += numero  
+
+print("La suma total es:", suma)
+print("¡La suma superó 100! Programa terminado.")
+
+
